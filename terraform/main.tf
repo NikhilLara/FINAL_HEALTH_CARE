93cac356ac0972df55a296212cac4ffd23c6d525
NIKHIL LARA <nikhillara@gmail.com>
2024-09-11 22:08:55 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,42 @@ provisioner "remote-exec" {
 tags = {
   Name = "final-healthcare-server"
   }
+provisioner "remote-exec" {
+    inline = [
+"sudo apt-get update -y",
+"sudo apt-get install docker.io -y",
+"sudo systemctl restart docker",
+"docker run -itd -p 8084:8082 nikhillara1989/final_health_care:1.0"
+"sudo apt-get update -y",
+“sudo apt: pkg={{ item }} state=present
+    with_items:
+      - apt-transport-https
+      - ca-certificates
+      - curl
+      - software-properties-common”,
+“sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -”,
+“sudo Add Kubernetes apt repository
+    apt_repository:
+      repo: deb https://apt.kubernetes.io/ kubernetes-xenial main
+      distribution: xenial
+      components: main
+      state: present
+  “sudo apt: pkg={{ item }} state=present”
+    with_items:
+      - kubectl
+      - kubelet
+      - kubeadm
+“Sudo shell: kubeadm init --pod-network-cidr=10.244.0.0/16”
+Join Kubernetes cluster (optional)
+    shell: kubeadm join --token {{ output.stdout }} --discovery-token-ca-cert-hash {{ output.stdout }}
+    when: inventory_hostname in groups['k8s_masters']  # Assuming 'k8s_masters' group for master node
+****
+"sudo groupadd docker",
+"sudo usermod -aG docker ubuntu", 
+]
+
+
+
 provisioner "local-exec" {
   command = "echo ${aws_instance.final-healthcare-server.public_ip} > inventory"
   }
