daaa7d182306128dde9a32147ed9002b678caca9
NIKHIL LARA <nikhillara@gmail.com>
2024-09-12 13:49:13 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,35 +15,6 @@ provisioner "remote-exec" {
 tags = {
   Name = "final-healthcare-server1"
   }
-provisioner "remote-exec" {
-    inline = [
-"sudo apt-get update -y",
-"sudo apt-get install docker.io -y",
-"sudo systemctl restart docker",
-"docker run -itd -p 8084:8082 nikhillara1989/final_health_care:1.0",
-"sudo apt-get update -y",
-"sudo apt: pkg={{ item }} state=present",
-"sudo apt-get install apt-transport-https -y",
-"sudo apt-get install apt-ca-certificates -y",
-"sudo apt-get install apt-curl -y",
-"sudo apt-get install apt-ca-certificates -y",
-"sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -”,
-"sudo Add Kubernetes apt repository",
-"sudo apt-get install deb https://apt.kubernetes.io/ kubernetes-xenial main -y",
-"sudo apt-add repository distribution: xenial -y",
-"sudo apt-add repository components: main -y",
-"sudo apt add repository state: present -y",
-“sudo apt: pkg={{ item }} state=present -y”,
-"sudo apt-get install apt-kubectl -y",
-"sudo apt-get install apt-kubelet -y",
-"sudo apt-get install apt-kubeadm -y",
-"Sudo shell: kubeadm init --pod-network-cidr=10.244.0.0/16”,
-"Join Kubernetes cluster (optional)",
-"shell: kubeadm join --token {{ output.stdout }} --discovery-token-ca-cert-hash {{ output.stdout }}",
-"when: inventory_hostname in groups['k8s_masters']",  # Assuming 'k8s_masters' group for master node
-"sudo groupadd docker",
-"sudo usermod -aG docker ubuntu", 
-]
 provisioner "local-exec" {
   command = "echo ${aws_instance.final-healthcare-server1.public_ip} > inventory"
   }
