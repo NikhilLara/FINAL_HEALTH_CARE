436ff3f04eb667d04e5631a0a904b3e5a710831c
NIKHIL LARA <nikhillara@gmail.com>
2024-09-22 14:01:25 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,32 @@
+resource "tls_private_key" "healthcare_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_key_pem}' > ./pubkey.pem"
+  }
+}
+
+resource "aws_key_pair" "healthcare_key_pair" {
+  key_name   = var.keypair_name
+  public_key = tls_private_key.healthcare_private_key.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.healthcare_private_key.private_key_pem}' > ./private-key.pem"
+  }
+}
+
 resource "aws_instance" "final_healthcare_server_K8S_Master_node" {
-  ami           = "ami-0e86e20dae9224db8"
-  instance_type = "t2.medium"
-  key_name      = "keypair2"
-  vpc_security_group_ids = ["sg-0624c6a8b6420566d"]
+  ami                         = "ami-0e86e20dae9224db8"
+  instance_type               = "t2.medium"
+  key_name                    = aws_key_pair.healthcare_key_pair.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["sg-0624c6a8b6420566d"]
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("./keypair2.pem")
+    private_key = file("./private-key.pem")
     host        = self.public_ip
   }
 
@@ -29,15 +48,16 @@ resource "aws_instance" "final_healthcare_server_K8S_Master_node" {
 resource "aws_instance" "final_healthcare_server_K8S_worker_node" {
   count = 2
 
-  ami           = "ami-0e86e20dae9224db8"
-  instance_type = "t2.micro"
-  key_name      = "keypair2"
-  vpc_security_group_ids = ["sg-0624c6a8b6420566d"]
+  ami                         = "ami-0e86e20dae9224db8"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.healthcare_key_pair.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["sg-0624c6a8b6420566d"]
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("./keypair2.pem")
+    private_key = file("./private-key.pem")
     host        = self.public_ip
   }
 
