498eeb0bcd7788adf89f8a6377fb18010763743b
NIKHIL LARA <nikhillara@gmail.com>
2024-09-22 13:46:46 +0530

    Updated provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,9 +2,11 @@ provider "aws" {
 region = "us-east-1"
 }
 
-required_providers {
+terraform {
+  required_providers {
     ansible = {
-      version = "~> 1.3.0"
-      source  = "ansible/ansible"
+      source = "ansible/ansible"
+      version = "1.3.0"
     }
   }
+}
