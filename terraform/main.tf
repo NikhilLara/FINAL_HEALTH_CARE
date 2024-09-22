54dc6c1fd87edd27d3e3900a35027cbae8541588
NIKHIL LARA <nikhillara@gmail.com>
2024-09-22 13:34:58 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "final-healthcare-server-K8S-MASTER" {
+resource "aws_instance" "final_healthcare_server_K8S_Master_node" {
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
   key_name      = "keypair2"
@@ -17,8 +17,8 @@ resource "aws_instance" "final-healthcare-server-K8S-MASTER" {
   }
 
   tags = {
-    Name = "final-healthcare-server-K8S-MASTER"
-    Role = "control plane node"
+    Name = "final_healthcare_server_K8S_Master_node"
+    Role = "master node"
   }
 
   provisioner "local-exec" {
@@ -26,7 +26,7 @@ resource "aws_instance" "final-healthcare-server-K8S-MASTER" {
   }
 }
 
-resource "aws_instance" "final-healthcare-server-K8S-worker-node" {
+resource "aws_instance" "final_healthcare_server_K8S_worker_node" {
   count = 2
 
   ami           = "ami-0e86e20dae9224db8"
@@ -42,14 +42,45 @@ resource "aws_instance" "final-healthcare-server-K8S-worker-node" {
   }
 
   tags = {
-    Name = "final-healthcare-server-K8S-worker-node ${count.index}"
+    Name = "final-healthcare-server-K8S-worker-node-${count.index}"
     Role = "worker node"
   }
 
   provisioner "local-exec" {
     command = "echo 'worker-${count.index} ${self.public_ip}' >> ./files/hosts"
   }
+}
 
+resource "ansible_host" "final_healthcare_server_K8S_Master_node_host" {
+  depends_on = [
+    aws_instance.final_healthcare_server_K8S_Master_node
+  ]
+  name = "control_plane"
+  groups = ["master"]
+  variables = {
+    ansible_user = "ubuntu"
+    ansible_host = aws_instance.final_healthcare_server_K8S_Master_node.public_ip
+    ansible_ssh_private_key_file = "./private-key.pem"
+    node_hostname = "master"
+  }
+}
+
+resource "ansible_host" "final_healthcare_server_K8S_worker_node_host" {
+  depends_on = [
+    aws_instance.final_healthcare_server_K8S_worker_node
+  ]
+  count = 2
+  name = "worker-${count.index}"
+  groups = ["workers"]
+  variables = {
+    node_hostname = "worker-${count.index}"
+    ansible_user = "ubuntu"
+    ansible_host = aws_instance.final_healthcare_server_K8S_worker_node[count.index].public_ip
+    ansible_ssh_private_key_file = "./private-key.pem"
+  }
+}
+
+resource "null_resource" "ansible_playbook" {
   provisioner "remote-exec" {
     inline = ["ansible-playbook /var/lib/jenkins/workspace/Finance/terraform/ansibleplaybook.yml"]
   }
